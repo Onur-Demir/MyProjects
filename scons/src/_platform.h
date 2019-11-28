bf088f9e940df48e796c2484e2879e3785c2eaac
stmdevoloper <stmdevoloper@users.noreply.github.com>
2019-11-28 12:11:52 +0300

    dcommit

---
+++ scons/src/_platform.h
@@ -0,0 +1,7 @@
+// every project should have this file. It should include relevant header files for the platform (ie, CPU support files).
+
+// GD needs to define one of GD32F10X_MD GD32F10X_HD GD32F10X_XD GD32F10X_CL
+
+#define STM32L053xx
+
+

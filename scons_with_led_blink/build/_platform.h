e7c8e99e0c9ae679029186d10679f1b485ecca84
stmdevoloper <stmdevoloper@users.noreply.github.com>
2019-11-29 12:58:13 +0300

    gitnore_test

--- scons_with_led_blink/build/_platform.h
+++
@@ -1,9 +0,0 @@
-// every project should have this file. It should include relevant header files for the platform (ie, CPU support files).
-
-// GD needs to define one of GD32F10X_MD GD32F10X_HD GD32F10X_XD GD32F10X_CL
-
-#if !defined STM32L053xx
-#define STM32L053xx
-#endif
-
-
